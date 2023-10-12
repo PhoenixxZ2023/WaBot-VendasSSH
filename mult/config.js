ec4c9afb58ed95743a7c9e38cc39041ce8aa6927
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2023-10-12 13:57:45 -0300

    Update config.js

--- mult/config.js
+++ mult/config.js
@@ -1,7 +1,7 @@
 config = {
   dono:"559184035474", //seu número com 55+ddd sem o 9
-  tempo_teste:"3", //tempo do teste em horas
-  valorLogin: 0.10, //valor do login SSH
+  tempo_teste:"1", //tempo do teste em horas
+  valorLogin: 15, //valor do login SSH
   nomeLoja:"Loja teste", //nome da sua loja virtual
   linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
   token_mp:"TokenAqui" //seu token do mercado pago
